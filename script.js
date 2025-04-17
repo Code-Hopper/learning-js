ad13709a1d9c13eb42a12cab7f8d2a1de9e70db6
code-hopper <ameykhondekar01@gmail.com>
2025-04-17 20:30:11 +0530

    day 1

---
+++ script.js
@@ -0,0 +1,81 @@
+// let data = 123
+
+// // output statments
+
+// console.log(data)
+
+// console.log(12356)
+
+// console.log("This is some String")
+
+// console.log(true)
+
+// console.log(false)
+
+// console.log(null)
+
+console.log(10 + 20)
+
+let num1 = 20
+
+let num2 = 30
+
+console.log(num1 + num2)
+
+console.log(num1 / num2)
+console.log(num1 % num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+
+// concatenation 
+// joing data (strings)
+
+// "my age is" + 20
+
+// my age is 20
+
+// console.log("my age is " + 20)
+console.log("my age is" + " " + 20 + " and i code !")
+
+let string1 = "my age is"
+
+let age = 25
+
+let what_i_do = "i code !"
+
+console.log(string1 + " " + age + " " + what_i_do)
+
+// number + number = number
+// number + string = string
+// string + number = string
+// string + string = string
+
+let my_name = "i'm Om"
+
+let intro = "Hi , "
+
+console.log(intro + my_name + ". " + string1 + " " +age + " and " + what_i_do )
+
+// document.write("Hello from js !")
+
+document.write(intro + my_name + ". " + string1 + " " +age + " and " + what_i_do )
+
+// document.write( (50 + 50) / 2 )
+
+// it a blocking code
+// window.alert("hello from js again!")
+
+// alert("hello from js again!")
+
+// window.prompt("string")
+
+// console.log(window.prompt("enter your name !"))
+
+let username = " i'm " +  prompt("Enter your name !")
+let userage = prompt("Enter your age !")
+
+console.log(intro + username + ". " + string1 + " " + userage + " and " + what_i_do )
+
+document.write(intro + username + ". " + string1 + " " + userage + " and " + what_i_do )
+
+window.alert(intro + username + ". " + string1 + " " + userage + " and " + what_i_do )
