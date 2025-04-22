40ac815b3e9b3b8c77c1eecb3c4d3168b4ea2896
code-hopper <ameykhondekar01@gmail.com>
2025-04-22 20:13:19 +0530

    conditional statements

---
+++ script3.js
@@ -0,0 +1,83 @@
+// condition statement
+
+// if
+// if-else
+// nasted-if
+// if-elseif 
+// if-else-if ladder
+// switch case
+// ternary statement 
+
+// looping statement
+
+// for, while, do-while , forEach, for of for in, map, filter
+
+// wap in js to find out greater of 2 numbers
+
+// let num1 = 100;
+
+// let num2 = 20;
+
+// (num1 < num2) 
+
+// if(condition){
+//     // true condition 
+//     // then this block of code
+// }
+
+// continue
+
+// if(num1 < num2){
+//     console.log("num2 is greater than num1")
+// }
+
+// console.log("program completed !")
+
+// if(condition){
+// true block of code
+// } else{
+// false block of code
+// }
+
+// let num1 = 100;
+// let num2 = 200;
+
+// if(num1 < num2){
+//     console.log("num2 is greater than num1")
+// }else{
+//     console.log("num1 is greater than num2")
+// }
+
+// console.log("program completed !")
+
+// nasted-if
+
+// if(condtion){
+//     if(condition){
+//         if(condition){
+//             if(condition){
+
+//             }
+//         }
+//     }
+// }
+
+// wap to find out if a is greater than b , c and d 
+
+// let a = 31
+// let b = 20
+// let c = 30
+
+// if (a > b) {
+//     if (a > c) {
+//         if (a > d) {
+//             console.log("a is greater than all")
+//         } else {
+//             console.log("is a not greater")
+//         }
+//     } else {
+//         console.log("is a not greater")
+//     }
+// } else {
+//     console.log("is a not greater")
+// }
