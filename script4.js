dde293e429093075eda4777f972e2e704fe07564
code-hopper <ameykhondekar01@gmail.com>
2025-04-23 20:23:15 +0530

    conditional statment ternary statment

---
+++ script4.js
@@ -0,0 +1,116 @@
+// if(condition1){
+//     // if condition 1 is true then this block of code 
+// } else if(condition2) {
+//     // if condition 1 is false and condition2 then this block of code 
+// }else{
+//     // both conditions are false then this block of code 
+// }
+
+// wap in js to check if a is greater than b and c
+
+// wap in js to find greater of a, b and c
+
+// let a = 25
+// let b = 20
+// let c = 30
+
+// if(a > b){
+//     if(a > c){
+//         console.log("A is greatest of all number !")
+//     }else{
+//         console.log("C is greatest of all number !")
+//     }
+// }else if(b > c){
+//     console.log("B is greatest of all number !")
+// }else{
+//     console.log("C is greatest of all number !")
+// }
+
+// wap in js to find greater of a, b, c and d
+
+
+// switch case (menu driven program)
+
+// switch (choice) {
+//     case "choice 1": code
+//         break;
+//     .
+//     .
+//     .
+//     .
+//     .
+//     case "choice n": code
+//         break;
+//     default: code
+// }
+
+// let choice = 10
+
+// switch(choice){
+//     case 1: console.log("case 1 selected !")
+//     break;
+//     case 2: console.log("case 2 selected !")
+//     break
+//     case 3: console.log("case 3 selected !")
+//     break;
+//     case 4: console.log("case 4 selected !")
+//     break;
+//     case 5: console.log("case 5 selected !")
+//     break;
+//     default : console.log("invalid choice")
+// }
+
+// console.log("choose 1. addition 2. subtraction 3.multplication 4.division 5.modulus !")
+
+// let num1 = Number(prompt("enter value for num 1"))
+
+// let num2 = Number(prompt("enter value for num 2"))
+
+// let choice = Number(window.prompt("choose 1. addition 2. subtraction 3.multplication 4.division 5.modulus !"))
+
+// switch(choice){
+//     case 1 : console.log(num1+num2)
+//     break;
+//     case 2 : console.log(num1-num2)
+//     break;
+//     case 3 : console.log(num1*num2)
+//     break;
+//     case 4 : console.log(num1/num2)
+//     break;
+//     case 5 : console.log(num1%num2)
+//     break;
+//     default : console.log("invalid choice !")
+// }
+
+// vowel a e i o u
+
+// let char = "x"
+
+// switch (char) {
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//         console.log("entered charchter is a vowel")
+//     break;
+//     default : console.log("not a vowel") 
+// }
+
+// ternary statement
+
+// condition ? true block : false block 
+
+let a = 15
+let b = 10
+let c = 10
+
+// a > b ? console.log("a is greatest !") : console.log("not greatest !")
+
+// wap is js to check entred year is leap year or not 
+
+if(((a > b) && (a > c))){
+    console.log("a is greatest of all")
+}else{
+    console.log("a is not greatest")
+}
