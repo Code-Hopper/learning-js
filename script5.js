d74f5ae658c46699dc245d1f45e7cf7e076a7818
code-hopper <ameykhondekar01@gmail.com>
2025-04-29 20:50:02 +0530

    string manipulation

--- script5.js
+++ script5.js
@@ -1,289 +1,536 @@
-// looping statements
 
-// while do_while for
+// do_something()
+// do_something()
+// do_something()
 
-// while(condition){
-// if condition is true
-// updation in condition 
+
+// // looping statements
+
+// // while do_while for
+
+// // while(condition){
+// // if condition is true
+// // updation in condition 
+// // }
+
+// // while(true){
+// //     console.log("loading")
+// // }
+
+// // wap in js to print numbers from 0 to 10,000
+
+// // let number = 0
+
+// // while (number <= 10000) {
+
+// //     console.log(number)
+
+// //     number++
+// // }
+
+// // wap in js to print all even numbers from 0 to 10,000
+
+// // while (number <= 10000) {
+// //     if (number % 2 == 0) {
+// //         console.log(number)
+// //     }
+// //     number++
+// // }
+
+// // wap in js to print all odd numbers from 0 to 10,000
+
+// // while (number <= 10000) {
+// //     if (number % 2 != 0) {
+// //         console.log(number)
+// //     }
+// //     number++
+// // }
+
+// // let number = 10000
+
+// // // number != 0
+// // // number >= 0
+
+// // while(number >= -20){
+// //     console.log(number)
+// //     number--
+// // }
+
+// // leap year 
+
+// // 2000 - 2099
+
+// // let year = 2000
+
+// // let count = 0
+
+
+// // while ((year < 2100) && (count < 10) ) {
+
+// //     if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0)) {
+// //         count++
+// //         console.log(year + " year is leap year !")
+// //     }
+
+// //     year++
+// // }
+
+// // console.log("there were "+ count + " leap years !")
+
+// // factorials 
+
+// // 5! = 5 * 4 * 3 * 2 * 1 = 120
+
+// // let fact = 1
+
+// // let number = 5
+
+// // let org_number = number
+
+// // // fact = fact * digit (number 5)
+
+// // // fact = 1 * 5 (number--)
+
+// // // fact = 5 * 4
+
+// // // fact = 20 * 3
+
+// // // fact = 60 * 2
+
+// // // fact = 120 * 1
+
+// // // fact = 120 * 0
+
+// // while(number != 0){
+// //     fact = fact * number
+// //     number--
+// // }
+
+// // console.log("factorial of "+ org_number + " is : "+ fact)
+
+// // reverse number, sum of digits, check palindrome, amrstrong numbers
+
+// // forEeach, for of, filter, map (Array) 
+
+// // for in (object)
+
+// // topics
+
+// // working (client side / server side) & history
+
+// // interpreter 
+
+// // syntax : variable
+
+// // scope (var, let, const)
+
+// // naming rules for a variables
+
+// // data types
+
+// // primitive and non-primitive
+
+// // number, string, boolean, null, undefined, NaN
+
+// // objects, js-object (math, date), array,
+
+// // "123" -> 123
+
+// // "123A" -> NaN (Not a nunber)
+
+// // let data = null
+
+// // let data;
+
+// // let data = 123
+
+// // data = 456
+
+// // oprators
+// // airthmatic (+ - / * %), assignment ( =, +=,-=,*=,/=,%= ), increement/dcreement (++(+1), --(-1) | post, pre), comparision (< > <= >= != == ===), logical (&& , ||), bitwise (AND, OR), ternary ( ternary statements | ? )
+
+// // type casting
+// // number -> string
+// // number -> boolean
+// // string -> number
+// // string -> boolean
+// // boolean -> string
+// // boolean -> number
+
+// // let data = 10
+
+// // data = data + 5
+
+// // data += 5
+
+// // data -= 5
+// // data *= 5
+// // data /= 5
+// // data %= 5
+
+// // data++/-- (post)
+// // ++/--data (pre)
+
+// // let x = 10
+
+// // let y = x++
+// // let y = ++x
+
+// // let y = x--
+// // let y = --x
+
+// // console.log(y)
+
+// // console.log(x)
+
+// // ternary statement syntax
+// // condition ? if true : if false
+
+// // if, if-else, if-elseif , nasted if, switch case, ternary statement
+
+// // looping statement
+
+// // while loop
+
+
+// // concatenation (joining 2 or more strings)
+
+// // console.log("hello " + "world")
+
+// // let value = 10
+// // let value2 = 20
+
+// // let datatype = typeof(value)
+
+// // console.log(datatype)
+
+// // console.log(typeof (value))
+
+// // console.log(value)
+
+// // // type cast number -> string
+
+// // value = String(value)
+
+// // console.log(typeof (value))
+
+// // console.log(value)
+
+// // console.log(value + value2)
+
+// // // string + number = string(concatenated)
+// // // string + string = string(concatenated)
+// // // number + string = string(concatenated)
+
+// // // number + number = nunber
+
+// // Number
+// // Boolean
+
+// // // int / float
+
+// // let number = 10.999999
+
+// // console.log(number)
+
+// // // number -> integer | 10.99999 -> 10
+
+// // number = parseInt(number)
+
+// // console.log(number)
+// // console.log(typeof(number))
+
+
+// // parseInt()
+// // parseFloat()
+
+// // do_while
+
+// // do{
+
+// // code 
+// // updation
+
+// // }while(condition)
+
+// // let number = 0
+
+// // do{
+// //     console.log(number)
+// //     number++
+// // }while(number < 10)
+
+// // let choice = false
+
+// // do{
+
+// //     console.log("welcome")
+
+// //     choice = window.confirm("do you wish to continue")
+
+// // }while(choice == true)
+
+// // console.log("program completed !")
+
+// // task 
+
+// // take numbers(2) as user input and perform airthmatics oprations
+
+// // greater of 2 / 3 / 4 | try using logical oprators.
+
+// // make 3 menu driven program (switch case)
+
+// // leap year logic using conditional statement | year must be user input
+
+// // grading based on marks using conditional statement 
+
+// // conditional statement by your choice with ternary oprator.
+
+// // create number list (numbers from a range | odd, even, multiple of 3)
+
+// // count using while loop (no. of leap years from a range)
+
+// // factorial , sun of digits, armstrong number using while loop 
+
+// // do you wish to continue using do-while
+
+// // year % 100 == 0 && year % 400 == 0
+
+// // year % 4 == 0
+
+// // let year = parseInt(window.prompt("Enter Year !"))
+
+// // // if (year % 100 == 0) {
+// // //     if (year % 400 == 0) {
+// // //         console.log("it's a leap year !")
+// // //     } else {
+// // //         console.log("it is not a leap year !")
+// // //     }
+// // // } else if (year % 4 == 0) {
+// // //     console.log("it's a leap year !")
+// // // } else {
+// // //     console.log("it is not a leap year !")
+// // // }
+
+// // if((year % 100 != 0 && year % 4 == 0) || (year % 400 == 0)){
+// //     console.log('it is a leap year !')
+// // }else{
+// //     console.log('it is not a leap year !')
+// // }
+
+
+// // let startYear = parseInt(window.prompt("Enter Start Year !"))
+
+// // let sYear = startYear
+
+// // let endYear = parseInt(window.prompt("Enter End Year !"))
+
+// // let eYear = endYear
+
+// // let count = 0
+
+// // while (startYear <= endYear) {
+
+// //     let year = startYear
+
+// //     if ((year % 100 != 0 && year % 4 == 0) || (year % 400 == 0)) {
+// //         count++
+// //     } 
+
+// //     startYear++
+
+// // }
+
+
+// // while (endYear >= startYear) {
+
+// //     let year = endYear
+
+// //     if ((year % 100 != 0 && year % 4 == 0) || (year % 400 == 0)) {
+// //         count++
+// //     } 
+
+// //     endYear--
+
+// // }
+
+// // console.log("from year " + sYear + " to " + eYear + " there are " + count + " leap years !")
+
+// // armstrong nunber
+
+// // let start = 0
+// // let end = 1000
+
+// // while (start <= end) {
+
+// //     let number = start
+// //     let org_number = number
+
+// //     // 1*1*1 + 2*2*2 + 3*3*3 = 36 
+
+// //     let sum = 0
+
+// //     while (number != 0) {
+
+// //         let d = number % 10
+
+// //         sum = sum + d * d * d
+
+// //         number = parseInt(number / 10)
+
+// //     }
+
+// //     if (sum == org_number) {
+// //         console.log(org_number + " is armstrong number !")
+// //     }
+
+// //     start++
+
+// // }
+
+// // let number = 131
+// // let org_number = number
+
+// // let rev = 0
+
+// // // rev = rev * 10 + d
+
+// // // rev = 3 * 10 + 2
+
+// // // rev = 32 * 10 + 1
+
+// // while(number != 0){
+
+// //     let d = number % 10
+
+// //     rev = rev * 10 + d
+
+// //     number = parseInt(number/10)
+// // }
+
+// // if(rev == org_number){
+// //     console.log("number is palidrome")
+// // }else{
+// //     console.log("number is not a palidrome")
+// // }
+
+// // let choice = 5
+
+// // switch(choice){
+// //     case 1 : console.log('case 1 selected !') 
+// //     break;
+// //     case 2 : console.log("case 2 is selected");
+// //     break;
+// //     default : console.log("invalid choice !")
+// // }
+
+// // functions 
+
+// // using function keyword
+
+// // function function_name( args [optional] ){
+//     // function body
+//     // return statement (optional)
+// // }
+
+// function do_something(){
+//     console.log("doing somthing !!!")
 // }
 
-// while(true){
-//     console.log("loading")
+// // arrow function (hoisted)
+
+// // scope name = (args[optional]) => {
+//     // function body
+//     // return statement (optional) 
+// // }
+
+// let greet = () => {
+//     console.log("hello user !")
 // }
 
-// wap in js to print numbers from 0 to 10,000
+// let x = greet
 
-// let number = 0
+// // calling function by name
+// // function_name(args[optional])
+// // function_name(args[optional])
+// // function_name(args[optional])
+// // function_name(args[optional])
 
-// while (number <= 10000) {
 
-//     console.log(number)
+// x()
+// x()
+// x()
+// x()
+// x()
+// greet()
+// // greet()
+// // greet()
+// // greet()
+// // greet()
+// // string, array & object
 
-//     number++
+// no args & no return
+// args & no return
+// no args &  return
+// args &  return
+
+// function greet(){
+//     console.log("hello user !")
 // }
 
-// wap in js to print all even numbers from 0 to 10,000
+// greet()
+// greet()
+// greet()
+// greet()
 
-// while (number <= 10000) {
-//     if (number % 2 == 0) {
-//         console.log(number)
+// function greet(firstName, lastName){
+//     console.log('hello !' + firstName + " " +lastName)
+// }
+
+// greet("Amey", "khondekar")
+// greet("Amey1", "khondekar1")
+// greet("Amey2", "khondekar2")
+// greet("Amey3", "khondekar3")
+// greet()
+
+
+// function returnSomething() {
+
+//     let data = 10
+
+//     return data
+// }
+
+// let result = returnSomething()
+
+// console.log(result)
+
+// console.log(returnSomething())
+
+// function add(num1, num2){
+//     return num1+num2
+// }
+
+// console.log(add(10,20))
+// console.log(add(100,20))
+// console.log(add(10,200))
+// console.log(add(2,20))
+
+
+// function findLeapYears(startYear, endYear) {
+//     let count = 0
+//     while (startYear <= endYear) {
+//         let year = startYear
+//         if ((year % 100 != 0 && year % 4 == 0) || (year % 400 == 0)) {
+//             count++
+//         }
+//         startYear++
 //     }
-//     number++
+//     return count
 // }
 
-// wap in js to print all odd numbers from 0 to 10,000
-
-// while (number <= 10000) {
-//     if (number % 2 != 0) {
-//         console.log(number)
-//     }
-//     number++
-// }
-
-// let number = 10000
-
-// // number != 0
-// // number >= 0
-
-// while(number >= -20){
-//     console.log(number)
-//     number--
-// }
-
-// leap year 
-
-// 2000 - 2099
-
-// let year = 2000
-
-// let count = 0
-
-
-// while ((year < 2100) && (count < 10) ) {
-
-//     if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0)) {
-//         count++
-//         console.log(year + " year is leap year !")
-//     }
-
-//     year++
-// }
-
-// console.log("there were "+ count + " leap years !")
-
-// factorials 
-
-// 5! = 5 * 4 * 3 * 2 * 1 = 120
-
-// let fact = 1
-
-// let number = 5
-
-// let org_number = number
-
-// // fact = fact * digit (number 5)
-
-// // fact = 1 * 5 (number--)
-
-// // fact = 5 * 4
-
-// // fact = 20 * 3
-
-// // fact = 60 * 2
-
-// // fact = 120 * 1
-
-// // fact = 120 * 0
-
-// while(number != 0){
-//     fact = fact * number
-//     number--
-// }
-
-// console.log("factorial of "+ org_number + " is : "+ fact)
-
-// reverse number, sum of digits, check palindrome, amrstrong numbers
-
-// forEeach, for of, filter, map (Array) 
-
-// for in (object)
-
-// topics
-
-// working (client side / server side) & history
-
-// interpreter 
-
-// syntax : variable
-
-// scope (var, let, const)
-
-// naming rules for a variables
-
-// data types
-
-// primitive and non-primitive
-
-// number, string, boolean, null, undefined, NaN
-
-// objects, js-object (math, date), array,
-
-// "123" -> 123
-
-// "123A" -> NaN (Not a nunber)
-
-// let data = null
-
-// let data;
-
-// let data = 123
-
-// data = 456
-
-// oprators
-// airthmatic (+ - / * %), assignment ( =, +=,-=,*=,/=,%= ), increement/dcreement (++(+1), --(-1) | post, pre), comparision (< > <= >= != == ===), logical (&& , ||), bitwise (AND, OR), ternary ( ternary statements | ? )
-
-// type casting
-// number -> string
-// number -> boolean
-// string -> number
-// string -> boolean
-// boolean -> string
-// boolean -> number
-
-// let data = 10
-
-// data = data + 5
-
-// data += 5
-
-// data -= 5
-// data *= 5
-// data /= 5
-// data %= 5
-
-// data++/-- (post)
-// ++/--data (pre)
-
-// let x = 10
-
-// let y = x++
-// let y = ++x
-
-// let y = x--
-// let y = --x
-
-// console.log(y)
-
-// console.log(x)
-
-// ternary statement syntax
-// condition ? if true : if false
-
-// if, if-else, if-elseif , nasted if, switch case, ternary statement
-
-// looping statement
-
-// while loop
-
-
-// concatenation (joining 2 or more strings)
-
-// console.log("hello " + "world")
-
-// let value = 10
-// let value2 = 20
-
-// let datatype = typeof(value)
-
-// console.log(datatype)
-
-// console.log(typeof (value))
-
-// console.log(value)
-
-// // type cast number -> string
-
-// value = String(value)
-
-// console.log(typeof (value))
-
-// console.log(value)
-
-// console.log(value + value2)
-
-// // string + number = string(concatenated)
-// // string + string = string(concatenated)
-// // number + string = string(concatenated)
-
-// // number + number = nunber
-
-// Number
-// Boolean
-
-// // int / float
-
-// let number = 10.999999
-
-// console.log(number)
-
-// // number -> integer | 10.99999 -> 10
-
-// number = parseInt(number)
-
-// console.log(number)
-// console.log(typeof(number))
-
-
-// parseInt()
-// parseFloat()
-
-// do_while
-
-// do{
-
-    // code 
-    // updation
-
-// }while(condition)
-
-// let number = 0
-
-// do{
-//     console.log(number)
-//     number++
-// }while(number < 10)
-
-// let choice = false
-
-// do{
-    
-//     console.log("welcome")
-
-//     choice = window.confirm("do you wish to continue")
-
-// }while(choice == true)
-
-// console.log("program completed !")
-
-// task 
-
-// take numbers(2) as user input and perform airthmatics oprations
-
-// greater of 2 / 3 / 4 | try using logical oprators.
-
-// make 3 menu driven program (switch case)
-
-// leap year logic using conditional statement | year must be user input
-
-// grading based on marks using conditional statement 
-
-// conditional statement by your choice with ternary oprator.
-
-// create number list (numbers from a range | odd, even, multiple of 3)
-
-// count using while loop (no. of leap years from a range)
-
-// factorial , sun of digits, armstrong number using while loop 
-
-// do you wish to continue using do-while
+// console.log(findLeapYears(2000,2100))
+// console.log(findLeapYears(1800,2100))
+// console.log(findLeapYears(2000,2010))
+// console.log(findLeapYears(2000,2025))
+// console.log(findLeapYears(2025,2100))
---
+++ script6.js
@@ -0,0 +1,81 @@
+// string , array & objects
+
+// primitive data type
+
+// string
+
+// let data = "JAVA script"
+
+// properties | methods
+
+// property
+
+// console.log(data.length) 
+
+// j a v a   s c r i p t
+// 0 1 2 3 4 5 6 7 8 9 10
+
+// methods
+
+// data = data.toLowerCase()
+// data = data.toUpperCase()
+
+// ASCII
+
+// console.log(data)
+
+// let email = window.prompt("Enter email address !")
+
+// if(email.toLowerCase() == "admin@123".toLowerCase()){
+//     console.log("email matched !")
+// }else{
+//     console.log("email not matched !")
+// }
+
+// let input = window.prompt("enter some string")
+
+// input.length > 10 ? console.log("string rejected !") : console.log('string accepted')
+
+// let data = "This a string!"
+
+// console.log(data.charAt(4))
+
+// // if (data.charAt(0).toLowerCase() == "J".toLowerCase()) {
+// //     console.log('not a valid string')
+// // }else{
+// //     console.log('valid string')
+// // }
+
+// let spaceCounter = (string) => {
+
+//     let count = 0
+
+//     let pointer = 0
+
+//     while (pointer <= (string.length - 1)) {
+
+//         if (string.charAt(pointer) == " ") {
+//             count++
+//         }
+//         pointer++
+//     }
+
+//     console.log("In the string '" + string + "' there are : " + count + " spaces !")
+
+//     return count
+// }
+
+// spaceCounter(data)
+// spaceCounter("java script")
+// spaceCounter("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+
+// let data = "This a a a string a !"
+
+// console.log(data.indexOf("a"))
+// console.log(data.lastIndexOf("a"))
+
+// if("Amey" < "Abhay"){
+//     console.log('Amey is smaller !')
+// }else{
+//     console.log('Abhay is smaller !')
+// }
