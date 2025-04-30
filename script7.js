81cef3d12b9cc38fa0a3b989867b1c9bb4205ba7
code-hopper <ameykhondekar01@gmail.com>
2025-04-30 20:31:08 +0530

    regex

---
+++ script7.js
@@ -0,0 +1,69 @@
+// let data = "   This is some string!   "
+
+// // slice ,trim and sub-string a string
+
+// data = data.trim()
+
+// // this function can remove leading & trailing spaces from a string
+
+// console.log(data.length)
+
+// let data = "JavaScript Language"
+
+// // data.slice(startIndex, endIndex)
+
+// console.log(data.slice(0,6))
+
+// wap in js to craete a function that can sperate firstname and lastname from a given name string.
+
+// Amey Khondekar
+
+// let nameSpliter = (username) => {
+
+//     username = username.trim()
+
+//     let firstName = username.slice(0, username.indexOf(" "))
+//     let lastName = username.slice(username.indexOf(" "), username.length)
+
+//     // console.log("given string was "+ username + " where firstname is "+ firstName+ " and name is "+ lastName)
+
+//     console.log(`given string was ${username} where first name is ${firstName} and last name is ${lastName} ${100+ 500 /2}`)
+
+// }
+
+// nameSpliter("Amey Khondekar")
+// nameSpliter("Om Kumar")
+
+// // template litral
+// ``-back-tick 
+
+
+// replace
+
+// let data = "t t t t t t t !"
+
+// // data = data.replace("t", "T")
+// // data = data.replace("this", "That")
+
+// data = data.replaceAll("t", "T")
+
+
+// console.log(data) 
+
+// wap in js to craete a function that converts first letter of words to capital
+
+// let name = "Amey khondekar"
+
+// name = name.replace(name.charAt(0), name.charAt(0).toUpperCase())
+
+// console.log(name)
+
+// array and objects
+
+// collection of element, that are stored in continous memory locations
+
+// let numbers = [1,2,3,4,5,6,7,8,9,10]
+
+// numbers[0] = 1000
+
+// console.log(numbers[0])
