2123d23affcd5aefef42c44a7e6a5963a3cdd52f
code-hopper <ameykhondekar01@gmail.com>
2025-05-07 20:40:15 +0530

    date class and built in objects

---
+++ script11.js
@@ -0,0 +1,53 @@
+// built in objects
+// Date , Math
+
+// date
+
+// syntax for date objects
+
+let date = new Date()
+
+// setter / getter
+
+// console.log(date.getTime())
+
+// 1 jan 1970 00:00:00 UTC
+
+// 20:21:00 07/05/2025
+
+// 1746629541802
+
+// 97
+// 98
+// 99
+// 00
+// 01
+// 02
+
+// 20:25:00 07/05/2025
+
+// 1746629702364
+
+// console.log(date.getDate())
+// console.log(date.getMonth())
+// console.log(date.getDay())
+// console.log(date.getMinutes())
+// console.log(date.getHours())
+// console.log(date.getSeconds())
+// console.log(date.toLocaleDateString())
+// console.log(date.toLocaleTimeString())
+
+let date1 = new Date("2025-05-25")
+
+let date2 = new Date("2025-05-07")
+// creating custom date using contructor methods/function
+
+// yyyy-mm-dd
+
+if(date1.getTime() < date2.getTime()){
+    console.log("date 2 is latest !")
+    console.log(date2.toLocaleDateString())
+}else{
+    console.log("date 1 is latest !")
+    console.log(date1.toLocaleDateString())
+}
