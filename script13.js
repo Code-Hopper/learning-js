7593744535b4caf0157c5aecf7557eb75fcc663b
code-hopper <ameykhondekar01@gmail.com>
2025-05-10 19:41:41 +0530

    js reision

---
+++ script13.js
@@ -0,0 +1,188 @@
+// js is a higher level programming language
+
+// low mid high
+
+// client side(Browser) / server side(Node.js)
+
+// web , mobile, desktop
+
+// syntax
+
+// variables
+
+// scope variable_name = value;
+
+// let, var, const
+
+// value has to be of a certain datatype
+
+// primtive & non-primtive
+
+// number, boolean, string, NaN, undefined
+
+// array, object, function
+
+// dyanmicaly typed 
+
+// let data = 123
+
+// int data = 123
+
+// let data;
+// console.log(data)
+
+// opration
+// oprators
+// airthmatic | +-/*%
+// assignment | = , +=, -=, *=,/=
+// condition  | < > <= >= != == ===
+// increement & dcreement | ++ , -- (post,pre)
+// logical | && , || , !
+// bitwise | AND , OR
+// Ternary Oprator | ?
+
+// statement /expresion 
+
+// if, if-else, if-else if, if-else if ladder, nasted if , switch case
+
+// while, do_while, for
+
+// ternary statement
+
+// condition ? true block : flase code
+
+// let data = 10
+
+// data < 10 ? console.log('data is less than 10') : console.log('data is not less than 10') 
+
+// output statement
+// console.log()
+// docuement.write()
+// window.alert()
+// console.error()
+// console.warn()
+// console.info()
+
+// input statement
+// window.prompt()
+
+// type casting
+// Number()
+// String() / .toString()
+// Boolean()
+
+// string 
+// update (toLowerCase(), toUpperCase(), replace, replaceAll), search, sub-string , slice, concatenation
+
+// let data = "this is some string"
+
+// data.charAt(0)
+
+// t
+
+// template litrals
+
+// let data = 1200
+
+// "tihs is some data " + data
+
+// `this is some data ${data}`
+
+// match pattern
+// regex
+// regex.test(input)
+
+// methods / function
+
+// syntax | using function keyword | arrow function
+// hoisted data
+
+// function function(args){
+// function 
+// return
+//  }
+
+// let function_name = (args) => {
+// function body
+// return
+// }
+
+// args , return
+// no args , no return
+// args , no return
+// no args , return
+
+// function_name(args)
+
+// function doSomthing(){
+//     return "doing something"
+// }
+
+// doSomthing()
+
+// function(){
+
+// }
+
+// ()=>{
+// }
+
+// object & array
+
+// {
+// key1:value1,
+// key2:value2
+// }
+
+// array
+
+// forEach, for of, filter, sort, map
+
+// r, c, f
+
+// let arr = [
+//     [
+//         [],[]
+//     ],
+//     [
+//         [],[]
+//     ]
+// ]
+
+// built-in  
+// Date, Math
+
+// let date = new Date()
+
+// date.
+
+// getter / setter
+
+// date.getDate()
+// date.getTime()
+// date.getFullYear()
+// console.log(date.getMonth())
+
+// Math.property
+// Math.methods()
+
+// Math.PI
+
+// Math.floor(9.999999)
+
+// // 9
+
+// Math.ceil(0.0000001)
+
+// // 1
+
+// Math.round(0.54)
+
+// Math.pow(number, exponent)
+
+// Math.sqrt(number)
+// Math.cbrt(number)
+
+// 0 to 1
+
+// Math.floor((Math.random() * 6) + 1)
